c8dc278efd6e826f7a414f2b6d0deb557872d88f
imJull <j.davis@unadeca.net>
2022-08-01 22:25:02 -0600

    added a little data array

--- src/data/Data.js
+++ src/data/Data.js
@@ -1,3 +1,33 @@
 export const data = [
-    
+    users = [
+        {
+            client_id: 1,
+            username: "Jay",
+            user_roll: "admin",
+            full_name: "Lil Jay",
+            state: true,
+            phone: "85697521",
+            email:"cloutlord@clx.es",
+            password: "393sffj",
+            img: "nadaa"
+        }
+    ],
+    clients = [
+        {
+            client_id: 1,
+            name:"Larry",
+            lastname: "Flair",
+            firstname: "Rick",
+            doc_type: "Dimex",
+            doc_number: "874425554",
+            country: "Belize",
+            birth_date: "5-7-2000",
+            phone: "987741155",
+            church: "Methodist",
+            college: "Unknow",
+            classification: "XL",
+            
+        },
+
+    ]
 ]
